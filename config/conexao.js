8755fd919e81ea0a2cd5b859b35ab20610d3a965
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2025-03-21 10:34:56 -0300

    banco local

--- config/conexao.js
+++ config/conexao.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-const conexao = await mongoose.connect("mongodb://aluno:aluno@ac-pcra3vd-shard-00-00.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-01.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-02.ajiapmy.mongodb.net:27017/?replicaSet=atlas-14e4b2-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
+const conexao = await mongoose.connect("mongodb://localhost:27017/banco");
 
 export default conexao;
