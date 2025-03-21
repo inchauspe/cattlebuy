9c3188e08483e80f27e437096c8b651e85819eec
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2025-03-21 10:44:22 -0300

    banco

--- config/conexao.js
+++ config/conexao.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-const conexao = await mongoose.connect("mongodb://localhost:27017/banco");
+const conexao = await mongoose.connect("mongodb://localhost:27017/cadastro");
 
 export default conexao;
